14e482868ea49a99361385eafdc0e22688f5d724
hy4yh <hy4yh@users.noreply.github.com>
2019-03-04 22:39:56 +0900

    rubocop実行

--- db/migrate/20190304055815_change_column_to_book.rb
+++ db/migrate/20190304055815_change_column_to_book.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeColumnToBook < ActiveRecord::Migration[5.2]
   def change
     change_column :books, :title, :string, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_03_04_055815) do
-
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
     t.string "memo", null: false
@@ -20,5 +21,4 @@ ActiveRecord::Schema.define(version: 2019_03_04_055815) do
     t.string "author"
     t.string "picture"
   end
-
 end
