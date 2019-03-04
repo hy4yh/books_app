dfe4f922bdf4e44e18c67a3305da17f16082bcbe
hy4yh <hy4yh@users.noreply.github.com>
2019-03-04 15:10:20 +0900

    add not null option to title,memo columns

---
+++ db/migrate/20190304055815_change_column_to_book.rb
@@ -0,0 +1,6 @@
+class ChangeColumnToBook < ActiveRecord::Migration[5.2]
+  def change
+    change_column :books, :title, :string, null: false
+    change_column :books, :memo, :string, null: false
+  end
+end
