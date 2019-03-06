af3bcf3bc4cfe35557bb29ce6474d35da2624bc3
hy4yh <hy4yh@users.noreply.github.com>
2019-03-06 12:32:25 +0900

    rails g migration AddColumnToBook user_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_094007) do
+ActiveRecord::Schema.define(version: 2019_03_06_032426) do
+
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
     t.string "memo", null: false
@@ -20,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_094007) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.string "picture"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +35,5 @@ ActiveRecord::Schema.define(version: 2019_03_05_094007) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
