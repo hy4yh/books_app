88008dc8a83edf6b37d20a79f43698260a79124c
hy4yh <hy4yh@users.noreply.github.com>
2019-03-07 18:10:00 +0900

    rails generate migration AddOmniauthColumnsToUsers provider uid, rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_032426) do
+ActiveRecord::Schema.define(version: 2019_03_07_090918) do
+
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
     t.string "memo", null: false
@@ -33,7 +32,10 @@ ActiveRecord::Schema.define(version: 2019_03_06_032426) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
