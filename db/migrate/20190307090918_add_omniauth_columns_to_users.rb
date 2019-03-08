7997f6faf1f973857a856aad993b4375ddf531ce
hy4yh <hy4yh@users.noreply.github.com>
2019-03-08 19:48:23 +0900

    delete not null option

--- db/migrate/20190307090918_add_omniauth_columns_to_users.rb
+++ db/migrate/20190307090918_add_omniauth_columns_to_users.rb
@@ -1,8 +1,6 @@
 class AddOmniauthColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :uid,      :string, null: false, default: ""
-    add_column :users, :provider, :string, null: false, default: ""
-
-    add_index :users, [:uid, :provider], unique: true
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
   end
 end
