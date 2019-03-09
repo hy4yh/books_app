a7f81ee73c699a055bf3070d2c4ca9b97c595c7c
hy4yh <hy4yh@users.noreply.github.com>
2019-03-09 17:02:04 +0900

    delete new_with_session method from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,25 +11,15 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  class << self
-    def find_or_create_for_oauth(auth)
-      find_or_create_by!(email: auth.info.email) do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.name = auth.info.name
-        user.email = auth.info.email
-        password = Devise.friendly_token[0..5]
-        logger.debug password
-        user.password = password
-      end
-    end
-
-    def new_with_session(params, session)
-      if user_attributes = session["devise.user_attributes"]
-        new(user_attributes) { |user| user.attributes = params }
-      else
-        super
-      end
+  def self.find_or_create_for_oauth(auth)
+    find_or_create_by!(email: auth.info.email) do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+      password = Devise.friendly_token[0..5]
+      logger.debug password
+      user.password = password
     end
   end
 end
