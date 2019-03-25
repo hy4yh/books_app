321a82a98e6e702f6b453c81cfe7efc79ef54a68
hy4yh <hy4yh@users.noreply.github.com>
2019-03-26 03:26:31 +0900

    ドライバーをchromeに指定,ヘルパークラス追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,19 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
+Capybara.configure do |capybara_config|
+  capybara_config.default_driver = :selenium_chrome
+  capybara_config.default_max_wait_time = 10 # 一つのテストに10秒以上かかったらタイムアウトするように設定しています
+end
+# Capybaraに設定したドライバーの設定をします
+Capybara.register_driver :selenium_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  # options.add_argument('headless') # ヘッドレスモードをonにするオプション
+  options.add_argument('--disable-gpu') # 暫定的に必要なフラグとのこと
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+Capybara.javascript_driver = :selenium_chrome
+include Warden::Test::Helpers #deviseのlogin_asメソッドを使用するため追加
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
