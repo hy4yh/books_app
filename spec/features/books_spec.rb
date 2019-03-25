3c792dcb395dde77fa908d706a63f81bb68d6291
hy4yh <hy4yh@users.noreply.github.com>
2019-03-26 03:29:04 +0900

    add feature spec

---
+++ spec/features/books_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.feature "Books", type: :feature do
+  fixtures :users, :books
+  scenario "show articles" do
+    user = users(:tarou)
+    login_as(user, :scope => :user)
+    visit root_path
+    expect(page).to have_content I18n.t("books.index.listing_books")
+  end
+
+  scenario "create article" do
+    user = users(:tarou)
+    login_as(user, :scope => :user)
+    visit new_book_path
+    fill_in "book_title", with: "テストタイトル"
+    fill_in "book_memo", with: "テストメモ"
+    fill_in "book_author", with: "山田太郎"
+    attach_file 'book_picture', "#{Rails.root}/spec/fixtures/book_picture.jpg"
+    expect{click_on I18n.t("helpers.submit.create", model: "Book")}.to change{Book.count}.by(1)
+  end
+
+  scenario "show article" do
+    user = users(:tarou)
+    login_as(user, :scope => :user)
+    visit "/books/#{books(:first).id}"
+    expect(current_path).to eq "/books/#{books(:first).id}"
+  end
+
+  scenario "update article" do
+    user = users(:tarou)
+    login_as(user, :scope => :user)
+    visit "/books/#{books(:first).id}/edit"
+    fill_in "book_title", with: "更新したタイトル"
+    click_on I18n.t("helpers.submit.update", model: "Book")
+    expect(page).to have_content I18n.t("books.update.success")
+  end
+
+  scenario "delete article" do
+    user = users(:tarou)
+    login_as(user, :scope => :user)
+    visit root_path
+    expect do
+      accept_confirm{click_on I18n.t("destroy")}
+      find_by_id("notice")
+    end.to change{Book.count}.by(-1)
+  end
+end
---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.feature "Sessions", type: :feature do
+  fixtures :users
+  scenario "login" do
+    users(:tarou)
+    visit root_path
+    fill_in "user_email", with: "tarou@example.com"
+    fill_in "user_password", with: "password"
+    click_button I18n.t("devise.sessions.new.sign_in")
+    expect(page).to have_content I18n.t("devise.sessions.signed_in")
+  end
+
+  scenario "logout" do
+    user = users(:tarou)
+    login_as(user, :scope => :user)
+    visit root_path
+    click_on "sign out"
+    expect(page).to have_content I18n.t("devise.failure.unauthenticated")
+  end
+
+  scenario "facebook omniauth sign up and login" do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] = nil
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+      provider: 'facebook',
+      uid: '123545',
+      info: {
+        name: 'mockuser',
+        email: 'test_omniauth@gmail.com' 
+      }
+    })
+    visit root_path
+    expect do
+      click_on I18n.t("devise.shared.links.sign_in_with_provider", provider: "Facebook")
+      find_by_id("notice")
+    end.to change{User.count}.by(1)
+  end
+end
